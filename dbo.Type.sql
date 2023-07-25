bc17112c04975d269d79200f082085d7ad820470
guizaoui <guizaoui@ELHAIBA_AINH>
2023-07-25 15:48:36 +0100

    alj alj alj

---
+++ dbo.Type.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Type] (
+    [Id]              INT           NOT NULL,
+    [typee]           VARCHAR (255) NULL,
+    [section]         VARCHAR (255) NULL,
+    [inspectiongroup] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
